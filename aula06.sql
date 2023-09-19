87b966745a89984aa1c961082d582b03e568bf4d
Igor Fonseca <fonsecaigr@gmail.com>
2023-09-19 19:43:20 -0300

    Update aula06.sql

--- aula06.sql
+++ aula06.sql
@@ -5,15 +5,20 @@ create table tbl_cliente (codigo_cliente integer PRIMARY KEY, nome text not null
 create table tbl_titulo (codigo_titulo integer primary key, titulo text not null, descricao text, categoria chk_categoria);
 create table tbl_livros (cod_livro integer PRIMARY KEY, codigo_titulo integer REFERENCES tbl_titulo(codigo_titulo), status chk_status DEFAULT 'DISPONIVEL');
 create table tbl_emprestimo (numero_emprestimo integer PRIMARY KEY, codigo_cliente integer REFERENCES tbl_cliente(codigo_cliente), codigo_livro integer REFERENCES tbl_livros(cod_livro));
+
 -- Inserir dados na tabela tbl_cliente
+
 INSERT INTO tbl_cliente (codigo_cliente, nome, cidade, endereco)
 VALUES
+
     (1, 'Joao Silva', 'Sao Paulo', 'Rua A, 123'),
     (2, 'Maria Santos', 'Rio de Janeiro', 'Av. B, 456'),
     (3, 'Pedro Almeida', 'Belo Horizonte', 'Rua C, 789'),
     (4, 'Ana Oliveira', 'Salvador', 'Av. D, 1011'),
     (5, 'Carlos Lima', 'Brasília', 'Rua E, 1213');
+
 -- Inserir dados na tabela tbl_titulo
+
 INSERT INTO tbl_titulo (codigo_titulo, titulo, descricao, categoria)
 VALUES
     (1, 'Aventuras Urbanas', 'Uma história emocionante', 'DRAMA'),
@@ -21,7 +26,9 @@ VALUES
     (3, 'Amor nas Estrelas', 'Um romance intergaláctico', 'DRAMA'),
     (4, 'Código Enigmatico', 'Segredos ocultos', 'COMEDIA'),
     (5, 'Histórias Perdidas', 'Contos esquecidos', 'DRAMA');
+
 -- Inserir dados na tabela tbl_livros
+
 INSERT INTO tbl_livros (cod_livro, codigo_titulo, status)
 VALUES
     (1, 1, 'ALUGADO'),
@@ -29,7 +36,9 @@ VALUES
     (3, 2, 'DISPONIVEL'),
     (4, 3, 'ALUGADO'),
     (5, 4, 'ALUGADO');
+
 -- Inserir dados na tabela tbl_emprestimo
+
 INSERT INTO tbl_emprestimo (numero_emprestimo, codigo_cliente, codigo_livro)
 VALUES
     (1, 1, 2),
